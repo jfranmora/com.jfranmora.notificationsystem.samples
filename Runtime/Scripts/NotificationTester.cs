50ee6dacfc8c048620f1e7ce5db1d7b881dc99ae
Juan Mora <juan.mora@mediatonic.co.uk>
2020-04-26 22:55:37 +0100

    AchievementNotification sample

--- Runtime/Scripts/NotificationTester.cs
+++ Runtime/Scripts/NotificationTester.cs
@@ -2,13 +2,36 @@
 
 public class NotificationTester : MonoBehaviour
 {
+	private const float NormalNotificationDuration = 5f;
+
+	[SerializeField] private Sprite achievementSprite;
+
 	private int count;
-	
+
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Alpha1))
 		{
-			NotificationManager.ShowNotification<TextNotification>(new TextNotificationData($"Notification {count++}"), 5);
+			TriggerTextNotification();
 		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			TriggerAchievementNotification();
+		}
+	}
+
+	private void TriggerTextNotification()
+	{
+		NotificationManager.ShowNotification<TextNotification>(
+			new TextNotificationData($"Notification {count++}"),
+			NormalNotificationDuration);
+	}
+
+	private void TriggerAchievementNotification()
+	{
+		NotificationManager.ShowNotification<AchievementNotification>(
+			new AchievementNotificationData($"Notification {count++}", achievementSprite),
+			NormalNotificationDuration);
 	}
 }
---
+++ Runtime/Scripts/Notifications/AchievementNotification.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct AchievementNotificationData : INotificationData
+{
+    public string Text { get; }
+    public Sprite Sprite { get; }
+
+    public AchievementNotificationData(string text, Sprite sprite)
+    {
+        Text = text;
+        Sprite = sprite;
+    }
+}
+
+public class AchievementNotification : BaseExampleNotification<AchievementNotificationData>
+{
+    [Header("References")]
+    [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI text;
+
+    protected override void ShowInternal(AchievementNotificationData notificationData)
+    {
+        base.ShowInternal(notificationData);
+
+        text.text = notificationData.Text;
+        image.sprite = notificationData.Sprite;
+    }
+}
